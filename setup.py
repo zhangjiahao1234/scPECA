0919f463616f24ab473a27402ed79f2ac98f3c87
zhangjiahao1234 <751265325@qq.com>
2024-01-22 00:51:49 +0800

    setup v1.0

--- setup.py
+++ setup.py
@@ -1,18 +1,33 @@
 from setuptools import setup, find_packages
+import os
+import shutil
+
+def extract_Prior():
+    if not os.path.exists('Prior'):
+        os.makedirs('Prior')
+        shutil.unpack_archive('Prior.tar.gz', '.', 'gztar')
+        shutil.move('Prior', 'scPECA/Prior')
+
+def custom_install():
+    extract_Prior()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='scPECA',
     version='1.0',
     author='Jiahao Zhang',
     author_email='zhangjiahao@amss.ac.cn',
-    url='https://zhuanlan.zhihu.com/p/26159930',
-    description=u'吃枣药丸',
-    packages=['jujube_pill'],
-    install_requires=[],
-    entry_points={
-        'console_scripts': [
-            'jujube=jujube_pill:jujube',
-            'pill=jujube_pill:pill'
-        ]
-    }
+    description='PECA2 gene regulatory network construction for single-cell data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/zhangjiahao1234/scPECA',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # List of your package dependencies
+    ],
+    data_files=[('scPECA', ['Prior.tar.gz']), ('scPECA', ['Cones/*'])],
+    cmdclass={'install': custom_install}
 )
